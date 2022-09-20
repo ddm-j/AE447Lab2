70a2c1db12e80c44513a051994f5183bb825941c
Brandon <denbjornen505@gmail.com>
2022-09-20 00:34:13 -0600

    semi-working

--- coefficients.py
+++ coefficients.py
@@ -34,10 +34,10 @@ def differential_area(x, d_af, d='drag', c=0.0254*6, S=0.0254*24, surface='upper
     u = ux if d == 'drag' else uy
     d = -1 if d == 'lift' and surface == 'lower' else 1
 
-    return u*d*dA
+    return d*dA*u
 
 
-def force(x_c, p, d_a_int, limits, type='lift', method='simps'):
+def force(x_c, p, af, limits, type='lift', method='simps'):
 
     N = CoordSys3D('N')
 
@@ -45,35 +45,37 @@ def force(x_c, p, d_a_int, limits, type='lift', method='simps'):
     int_u = interp1d(x_c, p[:, 0], fill_value='extrapolate')
     int_l = interp1d(x_c, p[:, 1], fill_value='extrapolate')
 
-    # Define the integrand
-    if type == 'lift':
-        vec = N.j
-        d = -1
-    elif type == 'drag':
-        vec = N.i
-        d = 1
-    else:
-        raise ValueError('Unreocgnized force type.')
-
     # Differential Unit Area
-    dA_u = lambda t: differential_area(t, d_a_int, d=type, surface='upper')
-    dA_l = lambda t: differential_area(t, d_a_int, d=type, surface='lower')
+    dA = 24*0.0254*np.linalg.norm(af[1:] - af[:-1], axis=1)
 
-    # Interpolated Lambda Functions
-    I = lambda t: integrand(t, dA_u, int_u) + integrand(t, dA_l, int_l)
+    # Normal Vector
+    t = af[1:] - af[:-1]
+    n = np.zeros(t.shape)
+    n[:, 0] = t[:, 1]
+    n[:, 1] = -t[:, 0]
+    n /= np.linalg.norm(n, axis=1)[:, None]
 
-    if method == 'quad':
-        F = quad(I, limits[0], limits[1])[0]
-        #Fl = quadrature(Il, limits[0], limits[1])[0]
+    # Vector Area
+    dA_v = n*dA[:, None]
 
-    elif method == 'simps':
-        x_s = np.linspace(limits[0], limits[1], 20000)
-        F = simps(I(x_s), x_s)
-        #Fl = simps(Il(x_s), x_s)
-    else:
-        raise ValueError('Integration Method not supported.')
+    # Pressure Interpolation Points
+    x_int = af[1:, 0] + (af[1:, 0] - af[:-1, 0])/2
 
-    #F = Fl + Fu
+    # Upper Surface Forces
+    F_upper = dA_v*int_u(x_int)[:, None]
+    F_upper[1:, 1] *= -1
+
+    # Lower Surface Forces
+    F_lower = dA_v*int_l(x_int)[:, None]
+    #F_lower[:, 1] *= -1
+
+    # Total Forces
+    F = F_upper + F_lower
+
+    #plt.plot(af[:-1, 0], F[:, 1])
+    #plt.show()
+
+    F = np.sum(F, axis=0)
 
     return F
 
@@ -96,41 +98,35 @@ coeffs = {
 }
 airfoil = airfoil.subs(coeffs)
 
-# Log Discretize the Space
-x_l = np.geomspace(0.0001, coeffs['c_a'], num=1000)
+# Discretize the Space
+x_l = np.geomspace(0.00000001, coeffs['c_a'], num=1000)
 x_l[0] = 0.0
+
 
 # Get Airfoil Coordinates
 a_f = lambdify(x, airfoil.coeff(N.j))
-a_l = a_f(x_l)
+y_l = a_f(x_l)
 
-# Interpolate the Data to Remove the Tangent Discontinuity
-a_int = InterpolatedUnivariateSpline(x_l, a_l)
-d_a_int = a_int.derivative()
+# Create Differential Arc Length
+dx = np.diff(x_l)
+dy = np.diff(y_l)
+ds = np.sqrt(dx**2 + dy**2)
+S_l = np.insert(np.cumsum(ds), 0, 0.0)
+S = S_l[-1]
+
+# Get x values for equally spaced arc lengths
+S_int = interp1d(S_l, x_l)
+s_l = np.linspace(0.0, S, 1000)
+s_x = S_int(s_l)
+
+af = np.array(list(zip(s_x, a_f(s_x))))
+
 
 # Tap Locations
 x_tap = np.array(
     [0.0, 0.210, 0.526, 0.842, 1.158, 1.474, 1.790, 2.422, 2.738, 3.054, 3.686, 4.002, 4.318, 4.634, 4.950, 5.265]
 )
 x_tap *= 0.0254
-
-#plt.plot(x_tap, p_data[:, 0])
-#plt.plot(x_tap, p_data[:, 1])
-#plt.show()
-
-#a_f = lambdify(x, airfoil.coeff(N.j))
-#n_fx = lambdify(x, unit_normal.coeff(N.i))
-#n_fy = lambdify(x, unit_normal.coeff(N.j))
-#x_n = np.linspace(0.00001, 6*0.0254, 50)
-#y_n = a_f(x_n)
-
-#n_x = n_fx(x_n)
-#n_y = n_fy(x_n)
-
-#plt.plot(x_n, y_n)
-#plt.quiver(x_n, y_n, n_x, n_y)
-#plt.axis('equal')
-#plt.show()
 
 vs = ['v1', 'v2']
 AoAs = [0, 4, 8, 12, 16]
@@ -148,31 +144,30 @@ coeff_data = {
     "v1": coeff_data,
     "v2": coeff_data
 }
-print(coeff_data)
 
 for file in files:
 
     # Import Test data
     alpha = file[-1]
-    p_data = 101325+np.load('pressure_data/{0}_{1}.npy'.format(file[0], alpha))
+    p_data = -1*np.load('pressure_data/{0}_{1}.npy'.format(file[0], alpha))
 
     # Calculate Aerodynamic Forces
-    Ls = force(x_tap, p_data, a_int, (0.0, coeffs['c_a']), type='lift')
-    Ds = force(x_tap, p_data, a_int, (0.0, coeffs['c_a']), type='drag')
-    Fs = np.array([Ls, Ds])
+    Fs = force(x_tap, p_data, af, (0.0, coeffs['c_a']), type='lift')
     alpha = np.deg2rad(alpha)
     R = np.array([
-        [np.cos(alpha), np.sin(alpha)],
-        [-np.sin(alpha), np.cos(alpha)]
+        [np.cos(alpha), -np.sin(alpha)],
+        [np.sin(alpha), np.cos(alpha)]
     ])
     F = np.matmul(R, Fs)
-    L = F[0]/(q[file[0]]*SA)
-    D = F[1]/(q[file[0]]*SA)
+    L = F[1]/(q[file[0]]*SA)
+    D = F[0]/(q[file[0]]*SA)
 
     # Store Results
     idx = AoAs.index(file[-1])
     coeff_data[file[0]][idx, 1] = L
     coeff_data[file[0]][idx, 2] = D
 
-plt.plot(coeff_data['v2'][:, 0], coeff_data['v2'][:, 2])
+plt.plot(coeff_data['v1'][:, 0], coeff_data['v1'][:, 1])
+plt.twinx()
+plt.plot(coeff_data['v1'][:, 0], coeff_data['v1'][:, 2])
 plt.show()
